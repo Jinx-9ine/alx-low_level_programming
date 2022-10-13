ea26b0afea13d0f98e01e417c7a223f96409ed37
Your Name <thatomashaba505@gmail.com>
2022-10-13 07:10:25 -0700

    Initial commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  *  main - Entry point
  *  Return: Always 0 (Success)
  */
-int maini(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
