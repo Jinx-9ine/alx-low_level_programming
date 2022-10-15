b1bb44c2a56b6b173d81db109ef6801177d2f36a
Your Name <thatomashaba505@gmail.com>
2022-10-14 18:30:13 -0700

    Initial commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main = Entry point
+ * main - Entry point
  * Return: Always 0 (Sucess)
  */
 int main(void)
@@ -16,5 +16,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	
 	return (0);
 }
